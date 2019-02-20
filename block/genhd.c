315646cf30a77a3dbbbb99bd3834f02aa51f97db
zhengbin <zhengbin13@huawei.com>
2019-02-20 21:27:05 +0800

    block: fix NULL pointer dereference in register_disk
    commit 4d7c1d3fd7c7eda7dea351f071945e843a46c145 upstream.
    
    If __device_add_disk-->bdi_register_owner-->bdi_register-->
    bdi_register_va-->device_create_vargs fails, bdi->dev is still
    NULL, __device_add_disk-->register_disk will visit bdi->dev->kobj.
    This patch fixes that.
    
    Signed-off-by: zhengbin <zhengbin13@huawei.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Jack Wang <jinpu.wang@cloud.ionos.com>

--- block/genhd.c
+++ block/genhd.c
@@ -634,6 +634,13 @@ exit:
 	while ((part = disk_part_iter_next(&piter)))
 		kobject_uevent(&part_to_dev(part)->kobj, KOBJ_ADD);
 	disk_part_iter_exit(&piter);
+
+	if (disk->queue->backing_dev_info->dev) {
+		err = sysfs_create_link(&ddev->kobj,
+			  &disk->queue->backing_dev_info->dev->kobj,
+			  "bdi");
+		WARN_ON(err);
+	}
 }
 
 /**
