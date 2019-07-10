e67831a28a7d8f4cf9ad454fc1964325b1774524
Patrick Daly <pdaly@codeaurora.org>
2019-07-10 16:12:22 -0700

    psi: Add emergency trigger
    Provide a hook for the page allocator to call into before
    declaring OOM.
    
    Change-Id: I70e1d19395f8cfba4b56d11446670dd191b0a39f
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4329,9 +4329,6 @@ retry:
 	if (costly_order && !(gfp_mask & __GFP_RETRY_MAYFAIL))
 		goto nopage;
 
-	if (order <= PAGE_ALLOC_COSTLY_ORDER && should_ulmk_retry())
-		goto retry;
-
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
 				 did_some_progress > 0, &no_progress_loops))
 		goto retry;
@@ -4348,6 +4345,8 @@ retry:
 				&compaction_retries))
 		goto retry;
 
+	if (order <= PAGE_ALLOC_COSTLY_ORDER && should_ulmk_retry())
+		goto retry;
 
 	/*
 	 * Deal with possible cpuset update races or zonelist updates to avoid
