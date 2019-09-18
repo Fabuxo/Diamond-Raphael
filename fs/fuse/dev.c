1a3d0e9ab09e59c0209b45e2a89479fd294e1ba7
Arnd Bergmann <arnd@arndb.de>
2019-09-18 21:58:16 +0200

    BACKPORT: fuse: unexport fuse_put_request
    This function has been made static, which now causes a compile-time
    warning:
    
    WARNING: "fuse_put_request" [vmlinux] is a static EXPORT_SYMBOL_GPL
    
    Remove the unneeded export.
    
    Fixes: 66abc3599c3c ("fuse: unexport request ops")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Change-Id: I342b2de36c85a16e72d2c1352720ee42976a56c7

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -309,7 +309,6 @@ void fuse_put_request(struct fuse_conn *fc, struct fuse_req *req)
 			fuse_request_free(req);
 	}
 }
-EXPORT_SYMBOL_GPL(fuse_put_request);
 
 static unsigned len_args(unsigned numargs, struct fuse_arg *args)
 {
